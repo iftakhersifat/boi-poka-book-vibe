31d0c57a1ff978ec8052231036c7d27bd0583d88
iftakhersifat <hossensifat615@gmail.com>
2025-04-18 01:02:38 +0600

    added updated sections

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='flex justify-around items-center mt-12 mb-12 bg-gray-200 h-[600px] p-4 rounded-xl container mx-auto'>
+        <div className='flex justify-around items-center mt-12 mb-12 bg-gray-200 h-[400px] p-4 rounded-xl container mx-auto'>
             <div>
-                <h1 className='font-bold text-6xl mb-5'>Books to freshen up <br /> your bookshelf</h1>
-                <button className='btn bg-[#23BE0A] mt-5 text-white rounded-lg px-8 py-8 font-bold'>View The List</button>
+                <h1 className='font-bold text-4xl mb-3'>Books to freshen up <br /> your bookshelf</h1>
+                <button className='btn bg-[#23BE0A] mt-3 text-white rounded-lg px-4 py-4 font-bold'>View The List</button>
             </div>
             <div>
-                <img className='w-[600px] rounded-2xl' src="/assets/books.jpg" alt="" />
+                <img className='w-[400px] rounded-2xl' src="/assets/books.jpg" alt="" />
             </div>
         </div>
     );
