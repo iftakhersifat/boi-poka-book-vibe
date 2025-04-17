6e44d1a7982fa8ea2814678f7738182e3da66e53
iftakhersifat <hossensifat615@gmail.com>
2025-04-18 01:23:07 +0600

    added box-card

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import { useLoaderData } from 'react-router';
+import Book from '../Book/Book';
 
 const Home = () => {
     const booksData = useLoaderData();
@@ -9,6 +10,11 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <h1 className='text-[28px] font-bold text-center mb-8'>Books</h1>
+            <div>
+                {
+                    booksData.map(data=><Book key={data.bookId} data={data}></Book>)
+                }
+            </div>
         </div>
     );
 };
